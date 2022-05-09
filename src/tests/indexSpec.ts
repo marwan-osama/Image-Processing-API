90647a200ffdad4b76692e09546cd8e2ae90eaaf
marwan osama <mo989989@gmail.com>
2022-05-09 08:00:26 +0200

    defining more function return types and parameter types

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -3,8 +3,8 @@ import app from "../index";
 
 const server = supertest(app);
 
-describe("Endpoint testing", () => {
-  it("Expect to get the api/images endpoint", async () => {
+describe("Endpoint testing", (): void => {
+  it("Expect to get the api/images endpoint", async (): Promise<void> => {
     const response = await server.get("/api/images");
     expect(response.status).toBe(200);
   });
--- src/tests/utilities/changeSizeSpec.ts
+++ src/tests/utilities/changeSizeSpec.ts
@@ -1,8 +1,8 @@
 import changeSize from "../../utilities/changeSize";
 import path from "path";
 
-describe("Testing image tranformation function", () => {
-  it("Expected to reject with Input file is missing error", async () => {
+describe("Testing image tranformation function", (): void => {
+  it("Expected to reject with Input file is missing error", async (): Promise<void> => {
     const returned = changeSize(
       path.resolve("assets/full/fjord123.jpg"),
       path.resolve("assets/thumb/fjord123_100x100.jpg"),
@@ -12,7 +12,7 @@ describe("Testing image tranformation function", () => {
     await expectAsync(returned).toBeRejectedWithError(/Input file is missing/);
   });
 
-  it("Expected to resolve without errors", async () => {
+  it("Expected to resolve without errors", async (): Promise<void> => {
     const returned = changeSize(
       path.resolve("assets/full/fjord.jpg"),
       path.resolve("assets/thumb/fjord_100x100.jpg"),
